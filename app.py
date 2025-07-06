aa57b1d7f9e7a80952ea450229090e8c6482d49a
Pragya Panwar <98818480+pragyapanwar23@users.noreply.github.com>
2025-07-06 16:27:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,14 @@
 import streamlit as st
-import pickle
 import pandas as pd
 from PIL import Image
-import requests
+from recommend import recommend  
 
-# Load data
+import pickle
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-# TMDB API function
-def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY_HERE&language=en-US"
-    response = requests.get(url)
-    data = response.json()
-    return f"https://image.tmdb.org/t/p/w500{data['poster_path']}" if data.get("poster_path") else ""
-
-# Recommendation logic
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movies = []
-    for i in distances[1:6]:
-        movie_id = movies.iloc[i[0]].movie_id  # ← This was your original working line
-        recommended_movies.append((movies.iloc[i[0]].title, fetch_poster(movie_id)))
-    return recommended_movies
-
-# Page layout
 st.set_page_config(layout="wide")
 
-# Styling
 st.markdown("""
 <style>
 .stApp {
@@ -47,25 +26,18 @@ h1 {
 </style>
 """, unsafe_allow_html=True)
 
-# App icon
 st.image("https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/fc/fe/36/fcfe363e-fd08-cc5f-c85c-f3d16367bb79/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg", width=150)
-
-# Title
 st.markdown("<h1>Movie Recommendation System</h1>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; font-size:18px;'>Please select from the existing library</p>", unsafe_allow_html=True)
 
-# Movie selector
 movie_name = st.selectbox("Type in the movie to get recommendations:", movies['title'].values)
 
-# Recommend button
 if st.button("Recommend"):
     recommendations = recommend(movie_name)
 
     if recommendations:
         for movie in recommendations:
             st.subheader(movie['title'])
-            if movie['poster']:
-                st.image(movie['poster'])
+            st.write(movie['overview'])
             st.markdown("---")
     else:
         st.warning("No recommendations found. Try another movie.")
