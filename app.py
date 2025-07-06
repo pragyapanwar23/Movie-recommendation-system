fda006b0ff2e082a8b36e4ffede0769aae933790
Pragya Panwar <98818480+pragyapanwar23@users.noreply.github.com>
2025-07-06 15:23:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import pickle
 import pandas as pd
 from PIL import Image
 
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 def recommend(movie):
+    if movie not in movies['title'].values:
+        return []
+
     index = movies.loc[movies['title'] == movie].index[0]
     distance = similarity[index]
     distance = sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])
@@ -16,36 +22,37 @@ def recommend(movie):
     return recommended_movies
 
 
-movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
-
-similarity = pickle.load(open('similarity.pkl', 'rb'))
-
 st.set_page_config(layout="wide")
 
 st.markdown("""
 <style>
 .stApp {
-    margin-top: -4rem; 
-        display: flex;
+    margin-top: -4rem;
 }
-.stApp img {  
-    width: 200px;  
+.stApp img {
+    width: 200px;
     display: flex;
-    margin-left:450px;
-}</style>
+    margin-left: auto;
+    margin-right: auto;
+}
+h1 {
+    text-align: center;
+}
+</style>
 """, unsafe_allow_html=True)
 
-st.image("https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/fc/fe/36/fcfe363e-fd08-cc5f-c85c-f3d16367bb79/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg", width=150)  # Adjust width as needed
+st.image("https://is5-ssl.mzstatic.com/image/thumb/Purple112/v4/fc/fe/36/fcfe363e-fd08-cc5f-c85c-f3d16367bb79/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg", width=150)
+st.markdown("<h1>Movie Recommendation System</h1>", unsafe_allow_html=True)
 
-st.markdown("""
-<h1 style="text-align: center;">Movie Recommendation System</h1>
-""", unsafe_allow_html=True)
-
-movie_name = st.selectbox("Type in the movie to get recommendations", movies['title'].values)
+movie_name = st.selectbox("Type in the movie to get recommendations:", movies['title'].values)
 
 if st.button("Recommend"):
     recommendations = recommend(movie_name)
-    for movie in recommendations:
-        st.write(movie['title'])
-        st.write(movie['overview'])
+
+    if recommendations:
+        for movie in recommendations:
+            st.subheader(movie['title'])
+            st.write(movie['overview'])
+            st.markdown("---")
+    else:
+        st.warning("No recommendations found. Try another movie.")
