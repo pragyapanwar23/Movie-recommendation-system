1c1b2a400d0a6f3f94dea2ff0cda4fe2851190a4
Pragya Panwar <98818480+pragyapanwar23@users.noreply.github.com>
2025-07-06 16:08:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,25 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import zipfile
+import os
 
-# Load data
+if not os.path.exists("similarity.pkl"):
+    with zipfile.ZipFile("similarity.zip", 'r') as zip_ref:
+        zip_ref.extractall()
+
 movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movie_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))  # Or load from a ZIP if you're using that
 
-# TMDB API function
+with open('similarity.pkl', 'rb') as f:
+    similarity = pickle.load(f)
+
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY_HERE&language=en-US"
     response = requests.get(url)
     data = response.json()
     return f"https://image.tmdb.org/t/p/w500{data['poster_path']}" if data.get("poster_path") else ""
 
-# Recommendation logic
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
@@ -25,10 +30,8 @@ def recommend(movie):
         recommended_movies.append((movies.iloc[i[0]].title, fetch_poster(movie_id)))
     return recommended_movies
 
-# Page config
 st.set_page_config(layout="centered")
 
-# Header image (icon)
 st.markdown(
     """
     <div style='text-align: center; margin-top: 30px;'>
@@ -38,16 +41,12 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Title
 st.markdown("<h1 style='text-align: center;'>Movie Recommendation System</h1>", unsafe_allow_html=True)
 
-# Instruction
 st.markdown("<p style='text-align: center; font-size:18px;'>Please select from the existing library</p>", unsafe_allow_html=True)
 
-# Movie select
 selected_movie = st.selectbox("Type in the movie to get recommendations:", movies['title'].values)
 
-# Button
 if st.button('Recommend'):
     recommendations = recommend(selected_movie)
 
