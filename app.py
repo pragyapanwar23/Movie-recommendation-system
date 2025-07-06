cbf806aec394ee3e8bf719af0d06f398e37557c1
Pragya Panwar <98818480+pragyapanwar23@users.noreply.github.com>
2025-07-06 16:21:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,79 +2,60 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import zipfile
+import os
 
-# Load movie data
-movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
+# Extract similarity.pkl from ZIP if not already extracted
+if not os.path.exists("similarity.pkl"):
+    with zipfile.ZipFile("similarity.zip", 'r') as zip_ref:
+        zip_ref.extractall()
 
-# Load similarity matrix from pickle or zip
+# Load movie dictionary and similarity matrix
+movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movie_dict)
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-# TMDB poster fetch
+# Function to fetch poster using TMDB API
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY_HERE&language=en-US"
     response = requests.get(url)
     data = response.json()
     return f"https://image.tmdb.org/t/p/w500{data['poster_path']}" if data.get("poster_path") else ""
 
-# Recommendation logic
+# Recommend function
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended = []
+    recommended_movies = []
     for i in distances[1:6]:
-        movie_id = movies.iloc[i[0]]['movie_id']
-        title = movies.iloc[i[0]]['title']
-        poster = fetch_poster(movie_id)
-        recommended.append((title, poster))
-    return recommended
+        movie_id = movies.iloc[i[0]]['movie_id']  # Assuming this column is correct
+        recommended_movies.append((movies.iloc[i[0]]['title'], fetch_poster(movie_id)))
+    return recommended_movies
 
-# Set page config
+# Page layout
 st.set_page_config(layout="centered")
 
-# Custom CSS styling
-st.markdown("""
-    <style>
-        .stApp {
-            margin-top: -3rem;
-        }
-        h1 {
-            text-align: center;
-        }
-        .movie-title {
-            text-align: center;
-            font-size: 22px;
-            margin-top: 10px;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-# Centered image icon
+# Local image (filmicon.png in the same folder)
 st.markdown(
     """
-    <div style='text-align: center; margin-top: 20px;'>
-        <img src='https://raw.githubusercontent.com/pragyapanwar23/Movie-recommendation-system/main/movie-icon.png' width='150'/>
+    <div style='text-align: center; margin-top: 30px;'>
+        <img src='filmicon.png' width='150'/>
     </div>
     """,
     unsafe_allow_html=True
 )
 
 # Title and instructions
-st.markdown("<h1>Movie Recommendation System</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Movie Recommendation System</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; font-size:18px;'>Please select from the existing library</p>", unsafe_allow_html=True)
 
-# Movie input
-movie_name = st.selectbox("Type in the movie to get recommendations:", movies['title'].values)
+# Dropdown for selecting movie
+selected_movie = st.selectbox("Type in the movie to get recommendations:", movies['title'].values)
 
 # Recommend button
-if st.button("Recommend"):
-    recommendations = recommend(movie_name)
-
-    if recommendations:
-        for title, poster in recommendations:
-            st.markdown(f"<div class='movie-title'>{title}</div>", unsafe_allow_html=True)
-            if poster:
-                st.image(poster)
-            st.markdown("---")
-    else:
-        st.warning("No recommendations found. Try another movie.")
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie)
+    for name, poster_url in recommendations:
+        st.markdown(f"### {name}")
+        if poster_url:
+            st.image(poster_url)
